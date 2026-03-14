6ec97f73769d7a9c7333639a60a19950c8de96d1
kprajapati0417-lang <kprajapati0417@gmail.com>
2026-03-14 13:44:55 +0530

    ok

---
+++ Assignment_2/Prog_1.java
@@ -0,0 +1,15 @@
+//Write a java program to display your Address. 
+package Assignment_2;
+
+import java.util.*;
+public class Prog_1 {
+    
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in); // create Scanner object
+        
+        System.out.print("My Name Is Kp");
+       
+    }
+    
+}
